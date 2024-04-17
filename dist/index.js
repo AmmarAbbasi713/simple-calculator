ed72f1140035df7b0025f0ef67655869a7dd976e
Ammar <ammarabbasi@gmail.com>
2024-04-16 18:44:03 -0700

    my first code

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstNunber" },
+    { message: "Enter your second number", type: "number", name: "secondNunber" },
+    {
+        message: "select one of operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["ADDITION", "SUBTRACTION", "MULTIPLICATION", "DIVISION"],
+    },
+]);
+// CODITIONAL STATEMENT
+if (answer.operator === "ADDITION") {
+    console.log(answer.firstNunber + answer.secondNunber);
+}
+else if (answer.operator === "SUBTRACTION") {
+    console.log(answer.firstNunber - answer.secondNunber);
+}
+else if (answer.operator === "MULTIPLICATION") {
+    console.log(answer.firstNunber * answer.secondNunber);
+}
+else if (answer.operator === "DIVISION") {
+    console.log(answer.firstNunber / answer.secondNunber);
+}
+else {
+    "Please select correct option";
+}
